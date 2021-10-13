c7c11d7a9a64144f0589af61ec3b1b3b9205d1cb
gabrielalimact <lgcl@ic.ufal.br>
2021-10-13 13:35:30 -0300

    rotas corrigidas

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -8,38 +8,49 @@ import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
 import { PlantSelect } from '../pages/PlantSelect';
-import AuthRoutes from './tab.routes';
+import AuthRoutes from './tabs.routes';
 
 const stackRoutes = createStackNavigator();
 
 const AppRoutes: React.FC = () => (
-    <stackRoutes.Navigator
-        headerMode = 'none'
-        screenOptions = {{
-            cardStyle: {
-                backgroundColor: colors.white
-            }
-        }}
-    >
-        <stackRoutes.Screen 
-            name='Welcome'
-            component={Welcome}
-        />
+  <stackRoutes.Navigator
+    headerMode="none"
+    screenOptions={{
+      cardStyle: {
+        backgroundColor: colors.white
+      }
+    }}
+  >
+   <stackRoutes.Screen
+    name="Welcome"
+    component={Welcome}
+   />
 
-        <stackRoutes.Screen 
-            name='UserIdentification'
-            component={UserIdentification}
-        />
+  <stackRoutes.Screen
+    name="UserIdentification"
+    component={UserIdentification}
+   /> 
 
-        <stackRoutes.Screen 
-            name='Confirmation'
-            component={Confirmation}
-        />
-        <stackRoutes.Screen 
-            name='PlantSelect'
-            component={AuthRoutes}
-        />
-    </stackRoutes.Navigator>
+  <stackRoutes.Screen
+    name="Confirmation"
+    component={Confirmation}
+   />
+
+  <stackRoutes.Screen
+    name="PlantSelect"
+    component={AuthRoutes}
+   /> 
+
+  <stackRoutes.Screen
+    name="PlantSave"
+    component={PlantSave}
+   />
+
+  <stackRoutes.Screen
+    name="MyPlants"
+    component={AuthRoutes}
+   /> 
+  </stackRoutes.Navigator>
 )
 
 export default AppRoutes;
